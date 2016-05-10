c2fdd9809f492a68918da2a08fd600dbecc0ba39
bjh970913 <bjh970913@gmail.com>
2016-05-10 22:14:47 +0900

    fuck

--- 05_04/hash.py
+++ 05_04/hash.py
@@ -1,23 +1,63 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 # -*- codeing:utf-8 -*-
-# from math import sqrt, floor
+from math import sqrt, floor
 
 
 def hash(string):
     if len(string) == 1:
         string = ' ' + string
-    # sum_0 = 0
-    # sum_1 = 0
-    # sum_2 = 0
+    sum_0 = 0
+    sum_1 = 0
+    sum_2 = 0
 
-    # for i in xrange(len(string) - 1):
-    #     sum_0 += floor(sqrt(ord(string[i]) + ord(string[i + 1])))
-    #     sum_1 += (ord(string[i]) ** 2 / 2) + ord(string[i + 1]) ** 2
-    #     sum_2 += ord(string[i]) ** 3 + ord(string[i + 1]) ** 3
+    for i in xrange(len(string) - 1):
+        sum_0 += floor(sqrt(ord(string[i]) + ord(string[i + 1])))
+        sum_1 += (ord(string[i]) ** 2 / 2) + ord(string[i + 1]) ** 2
+        sum_2 += ord(string[i]) ** 3 + ord(string[i + 1]) ** 3
 
-    # return floor(sum_0 ** 2 + sum_1 ** 2 + sum_2 ** 2)
-    return string[0] #+ string[1]
+    return floor(sum_0 ** 2 + sum_1 ** 2 + sum_2 ** 2)
 
+
+def hash_2(string):
+    tableSize = 10000
+    h = 0
+
+    for i in string:
+        h += ord(i)
+        # print hex(h)
+        h += shl(h, 10)
+        # print hex(h)
+        h ^= shr(h, 6)
+        # print hex(h)
+
+    h += shl(h, 3)
+    h = cut(h)
+    # print hex(h)
+    h ^= shr(h, 11)
+    h = cut(h)
+    # print hex(h)
+    h += shl(h, 15)
+    h = cut(h)
+    # print hex(h)
+
+    return (h)
+
+
+def shr(num, bit):
+    num = num >> bit
+
+    return cut(num)
+
+
+def shl(num, bit):
+    num = num << bit
+
+    return cut(num)
+
+
+def cut(num):
+    num = hex(num).split('0x')[1]
+    return eval('0x' + num[-8:])
 
 if __name__ == '__main__':
     freq = {}
@@ -27,14 +67,16 @@ if __name__ == '__main__':
     words = words.split('\n')
     max_key = 0
     collide = 0
-    print 'tested on ' + str(len(words)) + ' words'
 
+    # hash_2('apple')
     for i in words[:-1]:
-        key = hash(i)
+        key = hash_2(i)
+        # if hash_2('apple') == hash_2(i):
+            # print "collide with "+i+" hash : "+ str(hash_2(i))
         if key in freq:
             freq[key] += 1
             if freq[key] >= 1:
-                print 'clide on ' + i
+                # print 'clide on ' + i
                 collide += 1
         else:
             freq[key] = 0
@@ -42,5 +84,5 @@ if __name__ == '__main__':
             max_key = freq[key]
 
     print freq
+    print 'tested on ' + str(len(words)) + ' words'
     print 'collide ' + str(collide) + ' times'
-    print 'maximun is ' + str(max_key)
--- 05_04/hw1.c
+++ 05_04/hw1.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
-{
-    int i, sum=0;
-    char buff[10];
-    for(i=0;i<4;i++)
-    {
-        printf("Enter an interger string: ");
-        gets(buff);
-        sum += atoi(buff);
-    }
+int main() {
+   char str[1000];
+   int i1, i2, cnt[100] = { 0, };
+   char temp[20][10];
+   printf("Enter three lines of text:\n");
+   fgets(str, sizeof(str), stdin);
+   fgets(str + strlen(str), sizeof(str), stdin);
+   fgets(str + strlen(str), sizeof(str), stdin);
+   char *context = NULL;
+   char *token = strtok(str, " .\n");
 
-    printf("\nThe total of the values is %d\n", sum);
-    return 0;
+   printf("\n");
+   for (i1 = 0; token != NULL; i1++) {
+      strcpy(temp[i1], token);
+      cnt[i1]++;
+      token = strtok(NULL, " .\n");
+      for (i2 = 0; i2 < i1; i2++) {
+         if (strcmp(temp[i1], temp[i2]) == 0) {
+            cnt[i2]++;
+            cnt[i1]--;
+         }
+      }
+   }
+
+   for (i1 = 0; i1 < 100; i1++) {
+      if (cnt[i1] == 1) printf("\"%s\" appeared %d time\n", temp[i1], cnt[i1]);
+      else if (cnt[i1] > 1) printf("\"%s\" appeared %d times\n", temp[i1], cnt[i1]);
+   }
 }
